0371da147b1e35b5e04319322ad5f17a7e80ec7f
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-17 17:06:15 +0530

    upd: - used .env to keep secrets safe - used middleware to make products images static - created route to get products

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 // import modules
 const express = require("express");
-const mysql = require("mysql");
+const path = require("path");
+const mysql = require("mysql2");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
@@ -12,35 +13,38 @@ const app = express();
 
 // db
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password', 
-  database: 'ecommerce',
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DATABASE_NAME,
 });
 
 db.connect(err => {
   if (err) {
     console.error('Error connecting to the database:', err);
   } 
-  else {
-    console.log('Connected to the database');
-  }
 });
 
 
 // middleware
-app.use(morgan("dev"));
+app.use(express.json());
 app.use(cors({ origin: true, credentials: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+
 
 // routes
+app.get("/products", (req, res) => {
+  const query = "SELECT * FROM products";
+  db.query(query, (error, results) => {
+    if (error)
+      res.status(500).json({ error: "Error Fetching Products"});
+    res.json(results);
+  });
+});
 
 
-
-// port
-const port = process.env.PORT;
 
 
 // listener
-const server = app.listen(port, () =>
-  console.log(`Server is running on port ${port}`)
-);
+app.listen(process.env.PORT);
