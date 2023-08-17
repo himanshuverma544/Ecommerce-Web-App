d0514a85b2e31939859ebca90e56c0e937715d88
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-17 17:08:18 +0530

    rm: removed the svg plugin

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-import svgr from "vite-plugin-eslint";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
@@ -10,12 +9,6 @@ export default defineConfig({
   base: "/Ecommerce-Web-App",
   plugins: [
     react(),
-
-    svgr({
-      svgrOptions: {
-        ref: true,
-      },
-    }),
 
     eslint({
       cache: false,
