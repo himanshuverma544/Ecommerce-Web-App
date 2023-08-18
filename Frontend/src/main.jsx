d4456c273602eff176ace1bd3473d8346c5f4316
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-18 23:57:28 +0530

    upd: wrapped the app for using react-query

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import { QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query';
+
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App/>
+    </QueryClientProvider>
+);
