b15a5859a55bfff482b5d46d6305172f3b5b5f65
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-18 23:55:00 +0530

    add: added the required constants

---
+++ Frontend/src/constants.js
@@ -0,0 +1,46 @@
+const HOME = {
+  name: "Home",
+  path: "/"
+};
+
+const CART = {
+  name: "Cart",
+  path: "/cart"
+};
+
+const CHECKOUT = {
+  name: "Checkout",
+  path: "/checkout"
+};
+
+const THANKYOU = {
+  name: "Thankyou",
+  path: "/thankyou"
+};
+
+const PAGE404 = {
+  name: "404",
+  path: "*"
+};
+
+
+const SIGN_UP = "Sign Up";
+const SIGN_IN = "Sign In";
+const SIGN_OUT = "Sign Out";
+
+
+const CURRENCY = "₹";
+const ADD_TO_CART_TEXT = "ADD TO CART";
+
+export {
+  HOME,
+  CART,
+  CHECKOUT,
+  THANKYOU,
+  PAGE404,
+  SIGN_UP,
+  SIGN_IN,
+  SIGN_OUT,
+  CURRENCY,
+  ADD_TO_CART_TEXT
+};
