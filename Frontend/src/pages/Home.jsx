199751e39a59859671d585a51d1b60e994ce6917
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-18 23:59:47 +0530

    upd: fetched the products from the api - displayed them in grid - used cards to show the products

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,81 @@
+import { useCallback, useMemo } from "react";
+
+import axios from "axios";
+import { useQuery } from "react-query";
+
+import { Container, Row, Col, Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+
+import { CURRENCY, ADD_TO_CART_TEXT } from "../constants";
+
+
+const Home = () => {
+
+  const fetchProducts = useCallback(async () => {
+
+    const URL = import.meta.env.VITE_GET_PRODUCTS_KEY;
+    const { data } = await axios.get(URL);
+    return data;
+
+  }, []);
+
+
+  const { isSuccess, data, isError, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: fetchProducts
+  });
+
+  if (isError) {
+    console.log(error);
+  }
+
+  const products = useMemo(() => {
+    if (isSuccess)
+      return data;
+  }, [isSuccess, data]);
+
+
+  return (
+    <>
+      {(isSuccess && products.length > 0) &&
+        <Container className="products-cont">
+          <Row>
+            <Col className="products-heading-col">
+              <h1>Buy Your Favourite Images</h1>
+            </Col>
+          </Row>
+          <Row>
+            {products.map(product => (
+              <Col key={product.product_id} className="products-col" md="4" lg="3">
+                <Card>
+                  <div className="product-image-cont">
+                    <img
+                      className="product-image"
+                      src={`${import.meta.env.VITE_SERVER_PATH}${product.product_image_url}`}
+                      alt={product.product_name}
+                    />
+                  </div>
+                  <CardBody className="product-card-body">
+                    <CardTitle tag="h5">
+                      {product.product_name}
+                    </CardTitle>
+                    <CardSubtitle
+                      className="mb-2 text-muted"
+                      tag="h6"
+                    >
+                      {`${CURRENCY} ${product.price}`}
+                    </CardSubtitle>
+                    <Button className="add-to-cart-btn">
+                      {ADD_TO_CART_TEXT}
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      }
+    </>
+  );
+}
+
+export default Home;
