98093186b9a218d4daa515d372003a311e5a677b
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-18 23:56:18 +0530

    upd: defined the routes of the pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Page404 from "./pages/Page404";
+
+import { HOME, CART, PAGE404 } from './constants';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HashRouter basename="">
+        <Routes>
+          <Route path={HOME.path} element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path={CART.path} element={<Cart/>}/>
+            <Route path={PAGE404.path} element={<Page404/>}/>
+          </Route>
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
