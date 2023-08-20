c01a1c8554656765f44135c8acf2f0e99e7600c5
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-20 12:32:24 +0530

    upd: wrote the functionality for the authentication modal

--- Frontend/src/pages/Layout.jsx
+++ Frontend/src/pages/Layout.jsx
@@ -1,11 +1,39 @@
+import { useState, useRef, useCallback } from "react";
+
 import { Outlet, NavLink } from "react-router-dom";
 
 import { Container, Button } from "reactstrap";
 
-import { HOME, CART, SIGN_IN } from "../constants";
+import AuthenticationModal from "../modals/AuthenticationModal";
+
+import { HOME, CART, SIGN_IN, ORDERS } from "../constants";
 
 
 const Layout = () => {
+
+  const [showAuthenticationModal, setShowAuthentication] = useState(false);
+  const authenticationModalNode = useRef(null);
+
+  const openAuthenticationModal = useCallback(() => 
+    setShowAuthentication(true), 
+  []);
+
+  const isClickedOutsideOfModal = useCallback((event, node) => {
+    if (node.current && !node.current.contains(event.target))
+      return true;
+    return false;
+  }, []);
+
+  const closeAuthenticationModal = useCallback(event => {
+
+    if (event.target.innerText === ORDERS.name)
+      setShowAuthentication(false);
+
+    else if (isClickedOutsideOfModal(event, authenticationModalNode))
+      setShowAuthentication(false);
+
+  }, [isClickedOutsideOfModal]);
+
 
   return (
     <>
@@ -20,7 +48,12 @@ const Layout = () => {
                 <NavLink to={CART.path}>{CART.name}</NavLink>
               </li>
               <li>
-                <Button className="sign-in-btn">{SIGN_IN}</Button>
+                <Button 
+                  className="sign-in-btn" 
+                  onClick={openAuthenticationModal}
+                >
+                  {SIGN_IN.name}
+                </Button>
               </li>
             </ul>
           </nav>
@@ -34,6 +67,13 @@ const Layout = () => {
           <h6>Unlock the Power of Visual Storytelling with these Snapshots</h6>
         </footer>
       </Container>
+
+      {showAuthenticationModal &&
+        <AuthenticationModal
+          modalNode={authenticationModalNode}
+          closeModal={closeAuthenticationModal}
+        />
+      }
     </>
   );
 }
