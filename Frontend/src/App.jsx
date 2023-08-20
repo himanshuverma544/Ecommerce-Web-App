1a6f0a84cc295bf9b585be73d81455098702e3ab
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-20 12:36:06 +0530

    upd: added routes for following: - checkout - orders - thankyou

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,9 +6,12 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import Thankyou from './pages/Thankyou';
 import Page404 from "./pages/Page404";
 
-import { HOME, CART, PAGE404 } from './constants';
+import { HOME, CART, CHECKOUT, ORDERS, THANKYOU, PAGE404 } from './constants';
 
 
 function App() {
@@ -20,6 +23,9 @@ function App() {
           <Route path={HOME.path} element={<Layout/>}>
             <Route index element={<Home/>}/>
             <Route path={CART.path} element={<Cart/>}/>
+            <Route path={CHECKOUT.path} element={<Checkout/>}/>
+            <Route path={ORDERS.path} element={<Orders/>}/>
+            <Route path={THANKYOU.path} element={<Thankyou/>}/>
             <Route path={PAGE404.path} element={<Page404/>}/>
           </Route>
         </Routes>
