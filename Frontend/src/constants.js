5ac09458526ff6c59087efe3efb5f27384d2c304
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-20 12:31:16 +0530

    upd: created the required constants

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,3 +1,5 @@
+import { signInText } from "./functions";
+
 const HOME = {
   name: "Home",
   path: "/"
@@ -13,6 +15,11 @@ const CHECKOUT = {
   path: "/checkout"
 };
 
+const ORDERS = {
+  name: "View Your Orders",
+  path: "/orders"
+};
+
 const THANKYOU = {
   name: "Thankyou",
   path: "/thankyou"
@@ -24,23 +31,50 @@ const PAGE404 = {
 };
 
 
-const SIGN_UP = "Sign Up";
-const SIGN_IN = "Sign In";
-const SIGN_OUT = "Sign Out";
+const SIGN_UP = {
+  name: "Sign Up",
+  heading: "Sign Up Yourself",
+  switchMsg: "Already have an account?",
+  switchName: signInText(),
+  show: true
+};
+
+const SIGN_IN = {
+  name: signInText(),
+  heading: "Sign In Yourself",
+  switchMsg: "Don't have an account?",
+  switchName: SIGN_UP.name,
+  show: false
+};
+
+const SIGN_OUT = {
+  name: "Sign Out",
+  switchMsg: "Thank you for shopping with us.",
+  switchName: "😊"
+};
+
+const TEXT_AS_PER_STATUS = {
+  [SIGN_UP.name]: SIGN_UP,
+  [SIGN_IN.name]: SIGN_IN,
+  [SIGN_OUT.name]: SIGN_OUT
+};
 
 
 const CURRENCY = "₹";
 const ADD_TO_CART_TEXT = "ADD TO CART";
 
+
 export {
   HOME,
   CART,
   CHECKOUT,
+  ORDERS,
   THANKYOU,
   PAGE404,
   SIGN_UP,
   SIGN_IN,
   SIGN_OUT,
+  TEXT_AS_PER_STATUS,
   CURRENCY,
   ADD_TO_CART_TEXT
 };
