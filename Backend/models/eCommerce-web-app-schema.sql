096824adf7511bfa810fa4202adb98ca0e3cda8c
Himanshu Verma <himanshuverma544@gmail.com>
2023-10-01 18:28:43 +0530

    updated schema of the database

--- Backend/models/eCommerce-web-app-schema.sql
+++ Backend/models/eCommerce-web-app-schema.sql
@@ -1,70 +1,92 @@
 CREATE TABLE `users` (
-  `user_id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255),
   `email` varchar(255) UNIQUE NOT NULL,
-  `role` varchar(255) DEFAULT "USER" COMMENT 'else ADMIN'
+  `password` varchar(255) NOT NULL COMMENT 'store in hash format',
+  `role` varchar(255) NOT NULL DEFAULT "USER" COMMENT 'else ADMIN'
 );
 
 CREATE TABLE `products` (
-  `product_id` integer PRIMARY KEY AUTO_INCREMENT,
+  `product_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `product_name` varchar(255) NOT NULL,
   `price` integer NOT NULL,
-  `arrived` datetime NOT NULL
+  `arrived` datetime NOT NULL DEFAULT "CURRENT_TIMESTAMP",
+  `product_image_url` varchar(255) NOT NULL
 );
 
 CREATE TABLE `carts` (
-  `cart_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `user_id` integer,
-  `created_date` datetime NOT NULL,
-  `status` varchar(255) DEFAULT "ACTIVE" COMMENT 'else PURCHASED'
+  `cart_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT "CURRENT_TIMESTAMP",
+  `status` varchar(255) NOT NULL DEFAULT "ACTIVE" COMMENT 'else PURCHASED'
 );
 
 CREATE TABLE `cart_items` (
-  `cart_item_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `cart_id` integer,
-  `product_id` integer,
-  `quantity` integer DEFAULT 1 COMMENT 'quantity >= 1'
+  `cart_item_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `cart_id` integer NOT NULL,
+  `product_id` integer NOT NULL,
+  `quantity` integer NOT NULL DEFAULT 1 COMMENT 'quantity >= 1'
 );
 
 CREATE TABLE `orders` (
-  `order_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `user_id` integer,
-  `order_date` datetime NOT NULL,
-  `status` varchar(255) DEFAULT "ACTIVE" COMMENT 'else PENDING or COMPLETED'
+  `order_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `cart_id` integer NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT "CURRENT_TIMESTAMP",
+  `status` varchar(255) NOT NULL DEFAULT "ACTIVE" COMMENT 'else PENDING or SHIPPED'
 );
 
 CREATE TABLE `order_items` (
-  `order_item_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `order_id` integer,
-  `product_id` integer,
-  `quantity` integer COMMENT 'cart items table quantity will be copied here',
-  `unit_price` integer NOT NULL
+  `order_item_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id` integer NOT NULL,
+  `product_id` integer NOT NULL,
+  `quantity` integer NOT NULL DEFAULT 1 COMMENT 'if qty > 1, then cart_items table value will be copied here.'
 );
 
 CREATE TABLE `payments` (
-  `payment_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `order_id` integer,
-  `payment_date` datetime NOT NULL,
+  `payment_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id` integer NOT NULL,
+  `payment_date` datetime NOT NULL DEFAULT "CURRENT_TIMESTAMP",
+  `payment_method` varchar(255) NOT NULL DEFAULT "Cash on Delivery",
   `payment_amount` integer NOT NULL,
-  `status` varchar(255) DEFAULT "PENDING" COMMENT 'else IN PROCESS, SUCCESSFUL or FAILED'
+  `status` varchar(255) NOT NULL COMMENT 'SUCCESSFUL, IN PROCESS or FAILED'
 );
 
 CREATE TABLE `shipping` (
-  `shipping_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `shipping_method` VARCHAR(255),
+  `shipping_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `shipping_method` varchar(255) NOT NULL,
   `shipping_amount` integer NOT NULL
 );
 
+CREATE TABLE `shipping_details` (
+  `shipping_details_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id` integer NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255),
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip` integer NOT NULL,
+  `mobile_number` integer
+);
+
 CREATE TABLE `coupons` (
-  `coupon_id` integer PRIMARY KEY AUTO_INCREMENT,
+  `coupon_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `coupon_code` varchar(255) UNIQUE NOT NULL,
-  `coupon_type` varchar(255) DEFAULT "FLAT" COMMENT 'else PERCENTAGE',
+  `coupon_type` varchar(255) NOT NULL DEFAULT "FLAT" COMMENT 'else PERCENTAGE',
   `coupon_discount` integer NOT NULL,
-  `coupon_min_spend` integer DEFAULT 0,
-  `coupon_max_spend` integer DEFAULT 2147483647,
+  `coupon_min_spend` integer NOT NULL DEFAULT 0,
+  `coupon_max_spend` integer NOT NULL DEFAULT 2147483647,
   `coupon_created` date NOT NULL,
-  `coupon_expire` date NULL
+  `coupon_expire` date
+);
+
+CREATE TABLE `order_details` (
+  `order_details_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id` integer NOT NULL,
+  `coupon_id` integer NOT NULL,
+  `shipping_id` integer NOT NULL
 );
 
 ALTER TABLE `carts` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
@@ -75,8 +97,18 @@ ALTER TABLE `cart_items` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`
 
 ALTER TABLE `orders` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
+ALTER TABLE `orders` ADD FOREIGN KEY (`cart_id`) REFERENCES `cart_items` (`cart_id`);
+
 ALTER TABLE `order_items` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 
 ALTER TABLE `order_items` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 
 ALTER TABLE `payments` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+ALTER TABLE `shipping_details` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+ALTER TABLE `order_details` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+ALTER TABLE `order_details` ADD FOREIGN KEY (`coupon_id`) REFERENCES `coupons` (`coupon_id`);
+
+ALTER TABLE `order_details` ADD FOREIGN KEY (`shipping_id`) REFERENCES `shipping` (`shipping_id`);
